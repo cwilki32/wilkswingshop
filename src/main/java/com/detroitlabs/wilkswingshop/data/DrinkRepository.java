38b04a4f049fe919b6ac59d540c76eed9db2b514
cwilki32 <collin.wilkins@detroitlabs.com>
2022-03-24 12:31:41 -0400

    Added logo to home button on nav and update color scheme

--- src/main/java/com/detroitlabs/wilkswingshop/data/DrinkRepository.java
+++ src/main/java/com/detroitlabs/wilkswingshop/data/DrinkRepository.java
@@ -13,9 +13,15 @@ public class DrinkRepository {
     private static final List<Drink> DRINK_LIST = Arrays.asList(
             new Drink("Wine", "House Red", 6),
             new Drink("Wine", "House White", 6),
-            new Drink("Beer", "Lager", 4),
-            new Drink("Beer", "IPA", 5),
-            new Drink("Beer", "Red Ale", 4)
+            new Drink("Beer", "Lager", 3.50),
+            new Drink("Beer", "IPA", 5.50),
+            new Drink("Beer", "Red Ale", 4.50),
+            new Drink("Soda", "Root Beer", 1.99),
+            new Drink("Soda", "Coke", 1.99),
+            new Drink("Soda", "Tea", 1.99),
+            new Drink("Soda", "Lemonade", 1.99),
+            new Drink("Water", "Water", 0)
+
 
     );
 
